eb8b82d6ac7987c3ba270c75530d37c322f7a501
Mosamma Sultana Trina <114928172+Trina-SE@users.noreply.github.com>
2023-06-18 19:50:14 +0600

    Add files via upload

---
+++ Combinatorial_Optimization/Dynamic_Programming/BreadCutting.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int cuts[10000];
+int dp[10000][10000];
+
+int optimal(int left,int right,int cuts[]){
+
+   if(left>right) return 0;
+   if(dp[left][right]!=-1) return dp[left][right];
+    int mi=INT_MAX,cost;
+
+      for(int i=left;i<=right;i++){
+        cost=cuts[right+1]-cuts[left-1]+optimal(left,i-1,cuts)+optimal(i+1,right,cuts);
+        mi=min(mi,cost);
+      }
+
+      return dp[left][right]=mi;
+
+}
+
+int main(){
+
+   int length;
+   cin>>length;
+   int cut;
+   cin>>cut;//5
+   int cuts[cut+2];
+   cuts[0]=0;
+     for(int i=1;i<=cut;i++){
+        cin>>cuts[i];
+     }
+   cuts[cut+1]=length;
+   memset(dp,-1,sizeof(dp));
+   cout<<"The minimum cost of cutting the bread is "<<optimal(1,cut,cuts);
+
+
+
+}
+
+
+
+/*
+Input:
+100
+3
+25 50 75
+output=200
+10
+3
+2 4 7
+output=20
+7
+4
+1 3 4 5
+output:16
+*/
---
+++ Combinatorial_Optimization/Dynamic_Programming/EditDistance_it.cpp
@@ -0,0 +1,120 @@
+//Iterative
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int dp[3000][3000],total;
+char arrow[3000][3000];
+int gl,cl;
+
+void operation(string s,string t)
+{     int i=gl-1,j=cl-1,cnt=0;
+      string p=s;
+        while(i>=0 && j>=0){
+            if(s[i]==t[j]){
+               i--;
+               j--;
+            }
+            else{
+                if(arrow[i][j]=='u'){
+                    cnt++;
+                    p.erase(p.begin()+i);
+                    cout<<cnt<<"."<<"Delete "<<i<<"th "<<s[i]<<"->"<<p<<"\n";
+                    i--;
+                }
+                else if(arrow[i][j]=='l'){
+                    cnt++;
+                    p.insert(p.begin()+j,t[j]);
+                    cout<<cnt<<"."<<"Insert "<<j<<"th "<<t[j]<<"->"<<p<<"\n";
+                    j--;
+                }
+                else if(arrow[i][j]=='d'){
+                    cnt++;
+                    p[i]=t[j];
+                    cout<<cnt<<"."<<"Replace "<<i<<"th "<<s[i]<<" by "<<t[j]<<"->"<<p<<"\n";
+                    i--;
+                    j--;
+                }
+            }
+
+        }
+
+}
+
+int main(){
+
+   string given,convert;
+   cin>>given>>convert;
+   given="*"+given;
+   convert="*"+convert;
+
+
+//"given string" to "convert string"
+
+   gl=given.size();
+   cl=convert.size();
+
+      for(int i=0;i<=cl;i++){
+        dp[0][i]=i;
+        arrow[0][i]='l';//l=left
+      }
+      for(int i=0;i<=gl;i++){
+        dp[i][0]=i;
+        arrow[i][0]='u';//u=upper
+      }
+
+ //make table
+
+    for(int i=1;i<gl;i++){
+        for(int j=1;j<cl;j++){
+            if(given[i]==convert[j]){
+                dp[i][j]=dp[i-1][j-1];
+                arrow[i][j]='d';//d=diagonal
+            }
+            else{
+                dp[i][j]=(min(dp[i-1][j-1],min(dp[i-1][j],dp[i][j-1])))+1;
+
+                  if((dp[i-1][j-1]<=dp[i-1][j])) {
+                      if((dp[i-1][j-1]<=dp[i][j-1])) arrow[i][j]='d';
+                      else arrow[i][j]='l';
+                  }
+                  else{
+                    if((dp[i-1][j]<=dp[i][j-1])) arrow[i][j]='u';
+                    else arrow[i][j]='l';
+                  }
+
+            }
+        }
+    }
+
+    total=dp[gl-1][cl-1];
+
+  /*  for(int i=0;i<gl;i++){
+        for(int j=0;j<cl;j++){
+            cout<<dp[i][j]<<arrow[i][j]<<" ";
+        }
+        cout<<"\n";
+    }*/
+     cout<<"Minimum operations need to convert "<<given<<" to "<<convert<<" is:";
+     cout<<dp[gl-1][cl-1]<<"\n";
+     cout<<"Operations are:\n";
+     operation(given,convert);
+
+}
+
+/*
+Input:
+heater
+speak
+output:
+Minimum operations need to convert heater to speak is:5
+Operations are:
+----------
+Input:
+abcdef
+azced
+output:
+Minimum operations need to convert heater to speak is:3
+Operations are:
+----------
+*/
